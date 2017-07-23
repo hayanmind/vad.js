4608765be3a8d93f0004eb5aabd3af89a78a8f5e
Daehoon Kim <ghsdh3409@gmail.com>
2017-07-23 18:09:56 +0900

    Temporal commit

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 import VAD from './lib/vad';
+import AV from 'av';
+import { FFT } from './node_modules/jsfft/dist/fft';
 
+const audioFileAsset = AV.Asset.fromFile("./micro-doc.wav");
+audioFileAsset.get('format', (format) => {
+    console.log(format);
+    const vadObject = new VAD({
+        sampleRate: format.sampleRate,
+        fftSize: 256,
+        bufferLen: 256, 
+    });
+    audioFileAsset.decodeToBuffer((buffer) => {
+        for (let i=256; i<buffer.length; i=i+256) {
+            const pcmBuffer = buffer.slice(0, i);
+            const fftBuffer = FFT(pcmBuffer);
+
+            // console.log(pcmBuffer);
+            // console.log(fftBuffer);
+
+            const vadClass = vadObject.processFrequencyData(fftBuffer.real);
+            console.log(vadClass);
+        }
+    });
+});
+
+/*
+audioFileAsset.decodeToBuffer((buffer) => {
+    console.log(buffer);
+});
+*/
+/*
 const vadObject = new VAD({
     sampleRate: 22500,
 });
@@ -12,3 +42,4 @@ for (var i = 0; i < 512; i++) {
 
 const vadClass = vadObject.processFrequencyData(frequencyDomainData);
 console.log(vadClass);
+*/
