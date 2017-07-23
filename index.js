dcff2cf0e9661e83dc0cb5aff9303652a17dd947
Jeungmin Oh <tangza@gmail.com>
2017-07-23 15:56:19 +0900

    Initial version

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import VAD from './lib/vad';
 
-const vadObject = new VAD({});
+const vadObject = new VAD({
+    sampleRate: 22500,
+});
 
-console.log(vadObject);
+const frequencyDomainData = [];
+
+for (var i = 0; i < 512; i++) {
+    frequencyDomainData.push(0.5);
+}
+
+const vadClass = vadObject.processFrequencyData(frequencyDomainData);
+console.log(vadClass);
