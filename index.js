073a79ade1f226a1de13ec1ce30d684d316bdbf5
Daehoon Kim <ghsdh3409@gmail.com>
2017-07-27 18:32:55 +0900

    Work the same with original code

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// https://webaudio.github.io/web-audio-api/#widl-AnalyserNode-maxDecibels
+
 import VAD from './lib/vad';
 import AV from 'av';
 
@@ -5,6 +7,8 @@ var fft = require('ndarray-fft');
 var blackman = require('scijs-window-functions/blackman');
 var ndarray = require('ndarray');
 var db = require('decibels/from-gain');
+
+const { FFT } = require('jsfft');
 
 const applyWindow = blackman;
 const audioFileAsset = AV.Asset.fromFile("./micro-doc.wav");
@@ -44,9 +48,20 @@ audioFileAsset.get('format', (format) => {
             const frequencyBinCount = vadObject.options.fftSize / 2;
             const floatFrequencyData = new Float32Array(frequencyBinCount);
             getFloatFrequencyData(floatFrequencyData, _fdata, frequencyBinCount);
-            console.log(pcmBuffer);
-            console.log(floatFrequencyData);
-            asfdafasf
+            
+            /*
+            console.log(buffer.length);
+            console.log(pcmBuffer.length);
+            console.log(pcmBuffer.slice(0,20));
+            console.log(floatFrequencyData.length);
+            console.log(floatFrequencyData.slice(0,20));
+            assf
+            
+            if (i === 1024) {
+                afsfsdf;
+            }
+            */
+
             const { vadClass, voiceTrend } = vadObject.processFrequencyData(floatFrequencyData);
             if (prevVadClass !== vadClass) {
                 console.log(
@@ -66,6 +81,7 @@ audioFileAsset.get('format', (format) => {
 
 const _capture = (pcmBuffer, _data, _fdata, _fftCount, fftSize, bufferSize, smoothingTimeConstant) => {
     var channelData =  Array.prototype.slice.call(pcmBuffer);
+    // console.log(channelData);
     
 	//shift data & ensure size
     _data = _data.concat(channelData).slice(-bufferSize);
@@ -89,14 +105,15 @@ const _capture = (pcmBuffer, _data, _fdata, _fftCount, fftSize, bufferSize, smoo
 		var inputIm = ndarray(new Float32Array(fftSize));
 
 		//do fast fourier transform
-		fft(1, inputRe, inputIm);
+        fft(1, inputRe, inputIm);
 
 		//apply smoothing factor
-		var k = Math.min(1, Math.max(smoothingTimeConstant, 0));
+        var k = Math.min(1, Math.max(smoothingTimeConstant, 0));
 
         //for magnitude imaginary component is blown away. Not necessary though.
 		for (var i = 0; i < fftSize; i++) {
-			_fdata[i] = k* _fdata[i] + (1 - k) * Math.abs(inputRe.get(i)) / fftSize;
+            _fdata[i] = k* _fdata[i] + (1 - k) * Math.sqrt(Math.pow(inputRe.get(i),2) + Math.pow(inputIm.get(i),2)) / fftSize;
+            // Math.abs(inputRe.get(i)) / fftSize;
         }
     }
     
@@ -104,15 +121,12 @@ const _capture = (pcmBuffer, _data, _fdata, _fftCount, fftSize, bufferSize, smoo
 } 
 
 const getFloatFrequencyData = function (arr, _fdata, frequencyBinCount) {
-    const minDecibels = -100;
-    const maxDecibels = -30;
+    // https://stackoverflow.com/questions/26241430/fft-values-of-webaudios-analysernode-are-outside-range
 
 	if (!arr) return arr;
-
-    var minDb = minDecibels, maxDb = maxDecibels;
     
 	for (var i = 0, l = Math.min(frequencyBinCount, arr.length); i < l; i++) {
-		arr[i] = Math.max(db(_fdata[i]), minDb);
+		arr[i] = db(_fdata[i]);
 	}
 
 	return arr;
